c70f517d9ac1c71032a4cf7775e6d21ace6b017b
rafael altieris <raphaelaltieris@gmail.com>
2023-02-14 16:33:31 -0300

    Structures of Repetition

--- Desktop/curso Java/MaratonaJava/src/academy/devdojo/maratonajava/introducao/Aula06EstruturasdeRepeticao01.java
+++ Desktop/curso Java/MaratonaJava/src/academy/devdojo/maratonajava/introducao/Aula06EstruturasdeRepeticao01.java
@@ -1,6 +1,6 @@
 package academy.devdojo.maratonajava.introducao;
 
-public class Aula06EstruturasdeRepetiçaõ01 {
+public class Aula06EstruturasdeRepeticao01 {
     public static void main(String[] args) {
         int count = 0;
         while (count < 10) {
